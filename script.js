48ae1130c70717caac3da2c0b98b4e12dfcabb9e
Pankaj Jha <pankajjhawork@gmail.com>
2024-11-15 14:49:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(!str)
+    {
+        return null;
+    }
+
+let frObj={}
+
+
+for (let i=0; i<str.length-1;i++)
+{
+    
+   if (! frObj[str.charAt(i)])
+   {
+    frObj[str.charAt(i)]=1;
+   }
+
+   else
+   {
+    frObj[str.charAt(i)]= frObj[str.charAt(i)]+1;
+   }
+
+
+}
+	for (let item in frObj)
+{
+    if(frObj[item]==1)
+    {
+        return item;
+    }
+     return null
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
